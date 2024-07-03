fc7ee4152d02b6bc340de2b6ef73e53822e60370
Danlce <williamcorne17@gmail.com>
2024-07-02 21:03:35 -0500

    test

--- CWHSemestral/src/grafica/BibliotecaGUI.java
+++ CWHSemestral/src/grafica/BibliotecaGUI.java
@@ -109,8 +109,8 @@ public class BibliotecaGUI extends JFrame {
         alquileres = new ArrayList<>();
 
         // Ejemplo de datos iniciales
-        Libro libro1 = new Libro("El Señor de los Anillos", "J.R.R. Tolkien");
-        Libro libro2 = new Libro("Cien años de soledad", "Gabriel García Márquez");
+        Libro libro1 = new Libro("El Señor de los Anillos", "J.R.R. Tolkien", "Fantasia", 1, 1954, "Editorial Tirant Lo Blanch", "ingles", 24.99);
+        Libro libro2 = new Libro("Cien años de soledad", "Gabriel García Márquez,", "Ficcion", 2, 1967, "Editorial Planeta Mexicana", "Espanol", 26.95);
         Cliente cliente1 = new Cliente("Juan Pérez", 1001);
         Cliente cliente2 = new Cliente("María Gómez", 1002);
         Alquiler alquiler1 = new Alquiler(libro1, cliente1);
